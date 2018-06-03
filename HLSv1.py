e66f42b09c1bd2a7c5171bbaac054151e96f7a9c
jhuckestein <jameshuckestein@juno.com>
2018-06-03 15:20:42 -0500

    Add files via upload
    This includes a working version which will go out on the internet, or on the local PC directory, and retrieve an m3u8 file.  The class hierarchy for Playlist, VariantPlaylist, and MasterPlaylist has been added.  Once the m3u8 file is retrieved it is loaded into a "content list", and the createPlaylist function creates either a master or variant playlist object.  Currently, nothing is done with the playlist, but a print function has been added which verifies the playlist.contents list correct.

--- HLSv1.py
+++ HLSv1.py
@@ -36,6 +36,50 @@ import requests
 ##Set up logging for the program
 logging.basicConfig(filename='Hlsv1.log', level=logging.DEBUG)
 
+
+##Class definitions for the playlist hierarchy
+class Playlist(object):
+	def accept(self, validator):
+		validator.visit(self)
+	## Other methods that act on playlist?
+		
+	def __str__(self):
+		return self.__class__.__name__
+		
+	content = []         # A list of the original content of the URL
+	suppliedURL = ""	 # The URL supplied by the command line or batch file
+
+class VariantPlaylist(Playlist):
+	# Has a list of URLs to media segments (or locally defined) that end in ".ts"
+	# Has #EXT-X-PLAYLIST-TYPE the playlist type (LIVE, EVENT, VOD)
+	# Has a header that starts with #EXTM3U
+	# Has #EXT-X-STREAM-INF tag to indicate next URL identifies a playlist file
+	# Has #EXT-X-STREAM-INF tag which indicates BANDWIDTH attribute
+	# Has #EXT-X-STREAM-INF tag which should have a CODECS attribute (RFC 6381)
+	# Has #EXT-MEDIA-SEQUENCE which tells the first URL.ts that appears in the file
+	# Has #EXT-X-TARGETDURATION which specifies the maximum media file duration
+	# Has #EXT-X-VERSION which is the compatibility version of the playlist file
+	# Has #EXT-X-ENDLIST in VOD and possibly in EVENT
+	placeholder = True
+	
+	
+class MasterPlaylist(Playlist):
+	# Has a header that starts with #EXTM3U
+	# Has a #EXT-X-STREAM-INF that has BANDWIDTH, RESOLUTION, & CODEC
+	# Line after above is the URL.ts
+	# Has a list of object references for each URL.ts media variant
+	# They have to be able to create objects of VariantPlaylist
+	
+	variantList = []
+
+class Visitor:
+    def __str__(self):
+        return self.__class__.__name__
+		
+class Validator(Visitor): pass
+class MasterValidator(Validator): pass
+class VariantValidator(Validator): pass
+
 ####################################
 #
 # This function is used to open a URL or file
@@ -78,35 +122,90 @@ def openURL(url):
 
 ####################################
 #
+# This function creates MasterPlaylist objects
+def createMaster(conList, uRL):
+	pList = MasterPlaylist()
+	for i in range(0, len(conList)):
+			pList.content.append(conList[i])
+	pList.suppliedURL = uRL
+	return pList
+	
+	#Note: eventually I will need to add other attributes
+	#regarding the Master including calls to create the
+	#variant objects using createVariant below.
+
+
+#
+# End of createMaster
+####################################
+
+####################################
+#
+# This function creates VariantPlaylist objects
+def createVariant(contenList, urL):
+	varList = VariantPlaylist()
+	for i in range(0, len(contenList)):
+			varList.content.append(contenList[i])
+	varList.suppliedURL = urL
+	return varList
+	
+	#Note: eventually I will need to add other attributes
+	#regarding Variant playlists etc.
+
+
+#
+# End of createVariant
+####################################
+
+####################################
+#
 # This function is used to create Playlist Objects.  A MasterPlaylist
 # will / can contain VariantPlaylist(s), but a VariantPlaylist will
 # only have media files listed.  Both are subclasses of Playlist.
 # This function creates both types.
 #
-def createPlaylist(rsrc, urlFlag, webFlag):
+def createPlaylist(rsrc, urlFlag, webFlag, URL):
+	
 	if webFlag == True:
-	# Resource can be loaded into an object
+	# Resource can be loaded into an object, but must be decoded
 		logging.info("++---------->> Entered createPlaylist for Web URL:")
-		#print("++-------->> Headers for the URL:  ", rsrc.headers)
-		#print("++-------->> Encoding for the URL:  ", rsrc.encoding)
-		#print("++-------->> Text for the URL:  ", rsrc.text)
-		#print("++-------->> Status code for the URL:  ", rsrc.status_code)
-		#print("++-------->> Content for the URL:  ", rsrc.content)
-		# I suspect that these resource fields have not been populated
-		# because I am downloading a .m3u8 file instead of an actual web
-		# page.  This comes across as raw text and will probably need to 
-		# be parsed or something.
-		
-		#The below works and creates a good file.  So, maybe retrieve
-		#the file from the web, and then create the object where all
-		#objects would be created from disk?
-		fileHandle = open('OutputResource.txt','w')
-		fileHandle.write(rsrc.decode('utf-8'))
-		fileHandle.close()
+		contents = rsrc.decode('utf-8')
+		print("contents =", contents)
+		contentList = list(contents.splitlines())
+		#Now we have the contents of the URL
+		logging.info("++---------->> Created contentList of length: %s", len(contentList))
 		
 	else:
-	# Resource is the filehandle
+	# Resource is the filehandle we got from openURL, and the lines can be read
 		logging.info("++---------->> Entered createPlaylist for File URL:")
+		contents = rsrc.read()
+		contentList = list(contents.split("\n"))
+		logging.info("++---------->> Created contentList of length: %s", len(contentList))
+		print("contentList is: ", contentList)
+		
+	#Now the contentList is populated, and we need to create either a VariantPlaylist
+	#or create a MasterPlaylist.  If we iterate through the contentList, and find a
+	#.m3u8 extension, this would identify the playlist as a Master.  Otherwise, the
+	#VariantPlaylist does not have any tag with .m3u8 in it.
+	
+	master = False
+	for i in range(0, len(contentList)):
+		if '.m3u8' in contentList[i]:
+			master = True
+		#print("contentList was ", str(i), ": ", contentList[i])
+	logging.info("++------------>> The playlist object was test to be Master: %s", str(master))
+	#print("Master test was: ", str(master))
+	
+	#Now we know whether the supplied resource was a Master or Variant playlist.  Next,
+	#we create the object where a MasterPlaylist will create it's own VariantPlaylist(s).
+	
+	if master:
+		playList = createMaster(contentList, URL)
+	else:
+		playList = createVariant(contentList, URL)
+		
+	return playList
+		
 		
 
 
@@ -144,9 +243,11 @@ def main(argv):
 		while execute:
 			#Here is where the command line interaction goes.
 			#It finishes up with a command line call to the user.
+			#urlCheck(boolean) tells whether the given URL ends with .m3u8
+			#webCheck(boolean) tells wheter the URL begins with http/https
 			# 1) open the URL
 			resource, urlCheck, webCheck = openURL(url)
-			print(resource)
+			#print(resource)
 			print("The valid check for URL was: ", urlCheck)
 			
 			# 2) Create objects from the "resource"
@@ -154,8 +255,11 @@ def main(argv):
 			# resource is populated.  If webCheck is False this
 			# is a file on the computer, and we need to read lines.
 			
-			playlist = createPlaylist(resource, urlCheck, webCheck)
-				
+			playlist = createPlaylist(resource, urlCheck, webCheck, url)
+			
+			#print out the contents of the newly created playlist for debugging
+			for i in range(0, len(playlist.content)):
+				print(str(i), " content =", playlist.content[i])
 			
 			
 			
